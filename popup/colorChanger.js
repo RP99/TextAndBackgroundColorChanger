1f0c75e5212b067c0f16603c91fdcb1d00ed0699
Rohit Pai <rohitpai743@gmail.com>
2016-12-23 20:47:34 +0530

    Update colorChanger.js

--- popup/colorChanger.js
+++ popup/colorChanger.js
@@ -1,4 +1,5 @@
-var colorChange = 'document.body.style.color = "#fff"';
+var colorChange = 'document.body.style.color = "#fff"',
+    backgroundColorChange = 'document.body.style.backgroundColor = "#000"';
 document.addEventListener("click", (e) => {
   if(e.target.classList.contains("textColorChange")) {
     browser.tabs.executeScript(null, {
