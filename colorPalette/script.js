9fa2c364e298c5738b7e91b142aa35cb3127ee32
Rohit Pai <rohitpai743@gmail.com>
2016-12-29 22:05:22 +0530

    Update script.js

--- colorPalette/script.js
+++ colorPalette/script.js
@@ -1,2 +1,15 @@
-document.body.style.color = "#fff";
-document.body.style.backgroundColor = "#000";
+/*document.body.style.color = "#fff";
+document.body.style.backgroundColor = "#000";*/
+
+
+var chosenColor;
+console.log("outside click event listener");
+document.addEventListener("click", (e) => {
+	document.body.style.backgroundColor = "#000";
+  if(e.target.classList.contains("color"){
+    var anchor = e.target;
+    console.log(e.target.value);
+    chosenColor = anchor.value
+    document.getElementById("selectedColor").innerHTML = chosenColor;
+  }
+}
