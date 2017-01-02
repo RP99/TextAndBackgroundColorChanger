8f9f736f62da1d10227831cb48ead40e643ccdb6
Rohit Pai <rohitpai743@gmail.com>
2017-01-02 18:50:52 +0530

    Update bckgcolor_script.js

--- colorPalette/bckgcolor_script.js
+++ colorPalette/bckgcolor_script.js
@@ -1,1 +1,30 @@
+var backgroundColorChange;
+var chosenBckgColor;
+var colors = ["#fff","#000","#abc","#00f","#f00","#0f0"];
+
+  var colorBox = document.createElement("tr");
+
+  for(let i = 0; i < colors.length; i++){
+    let colorElement = document.createElement("td");
+	let valueAttr = document.createAttribute("value");
+
+    colorElement.style.backgroundColor = colors[i];
+	valueAttr.value = colors[i];
+	colorElement.setAttributeNode(valueAttr);
+    colorElement.classList.add("bckgColor");
+    colorBox.appendChild(colorElement);
+  }
+  document.getElementById("bckgColorPalette").appendChild(colorBox);
+
+document.addEventListener("click", (e) => {
+  if(e.target.classList.contains("bckgColor")){
+    var anchor = e.target;
+    chosenBckgColor = anchor.getAttribute("value");
+    document.getElementById("selectedBckgColor").innerHTML = chosenBckgColor;
+    backgroundColorChange = 'document.body.style.backgroundColor = "' + chosenBckgColor + '"';
+    browser.tabs.executeScript({
+      code: backgroundColorChange
+    });
+  }
+});
 
